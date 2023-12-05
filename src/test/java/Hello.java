159f9f8715d4de974861efb8a514fd0b048aa501
les0720 <TheShuvaeva@mail.ru>
2023-12-05 10:12:59 +0300

    hello

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,8 +1,9 @@
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
 
-public class HelloWorldTest {
+import io.restassured.RestAssured;
+        import io.restassured.response.Response;
+        import org.junit.jupiter.api.Test;
+
+public class Hello {
     @Test
     public void testHelloWorld(){
         Response response = RestAssured
