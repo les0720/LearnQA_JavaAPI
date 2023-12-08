3f31fa5e15f664173b03d2fc19ead23623fc813f
les0720 <TheShuvaeva@mail.ru>
2023-12-09 01:24:31 +0300

    Hello Jonn parsing json (answer).

---
+++ src/test/java/HelloJonnparsing.java
@@ -0,0 +1,22 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+public class HelloJonnparsing {
+    @Test
+    public void testRestAssured(){
+        Map<String,String> params = new HashMap<>();
+        params.put("name","Jonn");
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get( "https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+
+        String answer = response.get("answer");
+        System.out.println(answer);
+    }
+}
