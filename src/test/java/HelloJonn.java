94543e9de77b85ff78a8b480fc86e18840bd666a
les0720 <TheShuvaeva@mail.ru>
2023-12-09 00:49:48 +0300

    Hello, world (html)

---
+++ src/test/java/HelloJonn.java
@@ -0,0 +1,16 @@
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloJonn {
+    @Test
+    public void testRestAssured(){
+        Response response = RestAssured
+                .given()
+                .queryParam("name","Jonn")
+                .get( "https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
