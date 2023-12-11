6c74a2e971d0491c1115fca09a8074b83a00e4b2
les0720 <TheShuvaeva@mail.ru>
2023-12-11 23:34:10 +0300

    get параметры (пустые).

---
+++ src/test/java/Helloparamstype.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+public class Helloparamstype {
+    @Test
+    public void testRestAssured() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+
+        response.prettyPrint();
+    }
+}
