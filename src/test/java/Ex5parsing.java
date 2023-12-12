0cc8ef6f42a8782cccd4c9c55197ff495f0b0ee5
les0720 <TheShuvaeva@mail.ru>
2023-12-12 17:29:56 +0300

    parsing messages

---
+++ src/test/java/Ex5parsing.java
@@ -0,0 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+public class Ex5parsing {
+    @Test
+    public void testRestAssured() {
+
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        ArrayList message = response.get("messages");
+        System.out.println (message.get(1));
+
+    }
+}
