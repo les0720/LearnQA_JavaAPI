bd78241ef31470972eac45d159b3eb34bfda6afe
les0720 <TheShuvaeva@mail.ru>
2023-12-23 01:12:04 +0300

    Прогон, 2 теста:200_404

---
+++ src/test/java/Junit3.java
@@ -0,0 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Junit3 {
+    @Test
+    public void testFor200 () {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map")
+                .andReturn();
+        assertEquals(200, response.statusCode(), "Unexpected status code");
+
+    }
+
+    @Test
+    public void testFor404 () {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map2")
+                .andReturn();
+        assertEquals(404, response.statusCode(), "Unexpected status code");
+
+    }
+}
