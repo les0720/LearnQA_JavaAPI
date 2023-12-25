d6a779f481a8a65c45bd4f39ed61dff3091c2b16
les0720 <TheShuvaeva@mail.ru>
2023-12-25 19:31:07 +0300

    True: string > 15

---
+++ src/test/java/Parametrized.java
@@ -0,0 +1,33 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Parametrized {
+    @Test
+    public void testHelloMethodWithoutName () {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        assertEquals("Hello, someone", answer, "The answer is not expected");
+
+    }
+
+    @Test
+    public void testHelloMethodWithName () {
+        String name = "Username";
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParam("name", name)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        assertEquals("Hello, " + name, answer, "The answer is not expected");
+
+    }
+}
