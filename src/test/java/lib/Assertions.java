5675d02e60c44141a17222ae63635b799d890e7c
les0720 <TheShuvaeva@mail.ru>
2023-12-28 12:12:16 +0300

    Assertions

---
+++ src/test/java/lib/Assertions.java
@@ -0,0 +1,17 @@
+package lib;
+
+import io.restassured.response.Response;
+
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Assertions {
+    public static void asserJsonByName(Response Response, String name, int expectedValue) {
+        Response.then().assertThat().body("$",hasKey(name));
+
+        int value = Response.jsonPath().getInt(name);
+        assertEquals(expectedValue,value,"JSON value is not equals to expected value");
+
+    }
+}
+
