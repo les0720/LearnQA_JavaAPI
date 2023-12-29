43cba7443c07408555c0cd962b831a07630f6392
les0720 <TheShuvaeva@mail.ru>
2023-12-29 19:37:25 +0300

    UserAgent

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.Assertions;
 import lib.BaseTestCase;
 import org.junit.jupiter.api.Test;
 
@@ -11,8 +12,10 @@ import java.util.Map;
 public class UserRegisterTest extends BaseTestCase {
     @Test
     public void testCreateUserWithExistingEmail() {
+        String email = "vincotov@example.com";
+
         Map<String, String> userData = new HashMap<>();
-        userData.put("email", "vincotov@example.com");
+        userData.put("email", email);
         userData.put("password", "123");
         userData.put("username", "learnqa");
         userData.put("firstName", "learnqa");
@@ -24,7 +27,8 @@ public class UserRegisterTest extends BaseTestCase {
                 .post("https://playground.learnqa.ru/api/user/")
                 .andReturn();
 
-        System.out.println(responseCreateAuth.asString());
-        System.out.println(responseCreateAuth.statusCode());
+        Assertions.assertResponseCodeEquals(responseCreateAuth,400);
+        Assertions.assertResponseTextEquals(responseCreateAuth,"Users with email '" + email + "' already exists");
+
     }
 }
